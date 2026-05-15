7e06e7da201b06e02cf67f80f80675bcec10d241
beatrice <munecaibo@gmail.com>
2026-05-15 19:50:26 +0800

    feat: add GitHub Pages deployment workflow and configure base path for build

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sdgsfushing/', // 依照你的倉庫名稱設定明確路徑
+  base: '/sdgsfushing/', // 必須與你的 GitHub 倉庫名稱完全一致
 })
